07886740ba9f38d26898f25fed96ce320b87b2ff
yeomin4242 <whereismysejong@naver.com>
2025-08-27 16:38:13 +0900

    Build: Impl global exception and layer's exception #24

--- src/main/java/org/certis/studyplatform/exception/DomainException.java
+++ src/main/java/org/certis/studyplatform/exception/DomainException.java
@@ -2,7 +2,7 @@ package org.certis.studyplatform.exception;
 
 /**
  * Domain Layer Exception
- * 
+ *
  * 도메인 모델, 비즈니스 규칙, 애그리게이트 관련 예외 처리
  * - 도메인 규칙 위반
  * - 애그리게이트 일관성 위반
@@ -11,19 +11,21 @@ package org.certis.studyplatform.exception;
  */
 public class DomainException extends BaseException {
     
+    private static final long serialVersionUID = 1L;
+
     public DomainException(ExceptionStatus status) {
         super(status);
     }
-    
+
     public DomainException(ExceptionStatus status, Throwable cause) {
         super(status, cause);
     }
-    
+
     public DomainException(ExceptionStatus status, String customMessage) {
         super(status, customMessage);
     }
-    
+
     public DomainException(ExceptionStatus status, String customMessage, Throwable cause) {
         super(status, customMessage, cause);
     }
-} 
+}
