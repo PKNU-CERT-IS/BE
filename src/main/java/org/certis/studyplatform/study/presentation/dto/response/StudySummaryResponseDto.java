bf644d3602a7db5aa751e7364a7ad90d4ebe2473
yeomin4242 <whereismysejong@naver.com>
2025-09-04 21:41:30 +0900

    Feat: Impl study CRUD in presentation layer #41

---
+++ src/main/java/org/certis/studyplatform/study/presentation/dto/response/StudySummaryResponseDto.java
@@ -0,0 +1,64 @@
+package org.certis.studyplatform.study.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.certis.studyplatform.member.domain.MemberGrade;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+/**
+ * Study Summary Response DTO
+ *
+ * 스터디 목록 조회 시 사용되는 요약 정보 응답 DTO
+ * Presentation Layer의 응답 DTO
+ */
+@Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudySummaryResponseDto {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String category;
+
+    private String subcategory;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private OffsetDateTime startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private OffsetDateTime endDate;
+
+    private String studyCreatorName;
+
+    private MemberGrade studyCreatorGrade;
+
+    private boolean isParticipantable;
+
+    private Integer currentParticipantNumber;
+
+    private Integer maxParticipantNumber;
+
+    private List<StudyAttachedResponseDto> attachedStudies;
+
+    // toString for logging
+    @Override
+    public String toString() {
+        return "StudySummaryResponseDto{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", studyCreatorName='" + studyCreatorName + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+}
