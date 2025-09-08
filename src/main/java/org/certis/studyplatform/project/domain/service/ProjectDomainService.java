cca50727bae7e40dbf2ad2a9deda958f2f5a9cff
minwoonggi <d34sda334@gmail.com>
2025-09-08 21:00:28 +0900

    Refactor: refact application.yml for swagger #48

--- src/main/java/org/certis/studyplatform/project/domain/service/ProjectDomainService.java
+++ src/main/java/org/certis/studyplatform/project/domain/service/ProjectDomainService.java
@@ -246,7 +246,7 @@ public class ProjectDomainService {
             MemberVo requesterMember = memberDomainService.getMemberVo(new GetMemberByIdQuery(requesterId));
             MemberRole requesterRole = requesterMember.role();
 
-            if (requesterRole.isStaffOrAbove(requesterRole)) {
+            if (MemberRole.isStaffOrAbove(requesterRole)) {
                 log.debug("Domain: Update permission granted - requester is staff or above: {}", requesterRole);
                 return;
             }
@@ -281,7 +281,7 @@ public class ProjectDomainService {
             MemberVo requesterMember = memberDomainService.getMemberVo(new GetMemberByIdQuery(requesterId));
             MemberRole requesterRole = requesterMember.role();
 
-            if (requesterRole.isStaffOrAbove(requesterRole)) {
+            if (MemberRole.isStaffOrAbove(requesterRole)) {
                 log.debug("Domain: Delete permission granted - requester is staff or above: {}", requesterRole);
                 return;
             }
