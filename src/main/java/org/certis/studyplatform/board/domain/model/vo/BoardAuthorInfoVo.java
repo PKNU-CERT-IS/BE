15c8cf776420b1d439e33b916f73e789a054b6a2
yeomin4242 <whereismysejong@naver.com>
2025-09-19 23:47:49 +0900

    Refactor: Return AuthorInfo for board detail info api #63

---
+++ src/main/java/org/certis/studyplatform/board/domain/model/vo/BoardAuthorInfoVo.java
@@ -0,0 +1,5 @@
+package org.certis.studyplatform.board.domain.model.vo;
+
+import org.certis.studyplatform.member.domain.MemberRole;
+
+public record BoardAuthorInfoVo(String name, MemberRole role) {}
